970f897e8b2e19a89cc089a5e99ccaf24bde0492
Lucas E. <espiet.l@valbion.com>
2026-05-06 15:01:50 +0200

    feat: add logged name to header

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject, signal} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AppRoutes } from '#app/app.routes';
 import {
@@ -11,6 +11,9 @@ import {
   LucideLayoutDashboard,
   LucideList, LucidePackage, LucideSettings, LucideShieldUser, LucideUtensils
 } from '@lucide/angular';
+import {selectMember} from '#core/store/auth/auth.selector';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {Store} from '@ngrx/store';
 
 interface NavItem {
   label: string;
@@ -30,8 +33,12 @@ interface NavItem {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
+  private readonly store = inject(Store);
+
   protected mobileMenuOpen = signal(false);
   protected readonly routes = AppRoutes;
+  protected readonly authedMember = this.store.selectSignal(selectMember);
+  protected readonly memberInitials = computed(() => `${this.authedMember()?.firstName[0] ?? ''}${this.authedMember()?.lastName[0] ?? ''}`);
 
   protected readonly navItems: NavItem[] = [
     { label: 'Tableau de bord', icon: LucideLayoutDashboard, route: AppRoutes.home },
