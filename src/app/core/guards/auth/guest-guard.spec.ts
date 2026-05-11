af3ac72ce8cc10df84f747b249d461993f4f6870
Lucas E. <espiet.l@valbion.com>
2026-05-11 11:34:45 +0200

    test: fix tests

--- src/app/core/guards/auth/guest-guard.spec.ts
+++ src/app/core/guards/auth/guest-guard.spec.ts
@@ -3,7 +3,7 @@ import { CanActivateFn } from '@angular/router';
 
 import { guestGuard } from './guest-guard';
 
-describe('guestGuard', () => {
+describe(guestGuard.name, () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
     TestBed.runInInjectionContext(() => guestGuard(...guardParameters));
 
